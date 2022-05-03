2b0d7b9b792466bf16a5efd322695e4eedb10bbc
EDyTor <91157109+EDyTor@users.noreply.github.com>
2022-05-03 11:47:39 +0200

    Finish testing of Coordinates class

--- src/test/java/coordinates/CoordinatesTest.java
+++ src/test/java/coordinates/CoordinatesTest.java
@@ -1,9 +1,17 @@
 package coordinates;
 
 import org.assertj.core.api.Assertions;
+import org.assertj.core.util.Streams;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 class CoordinatesTest {
+
+
     @Test
     void shouldThrowExceptionWhenCoordinatesAreDifferent() {
         // Given
@@ -14,5 +22,23 @@ class CoordinatesTest {
         Assertions.assertThatThrownBy(() -> coord1.calculateDistanceTo(coord2))
                 .as("Should throw exception when coordinates are not synchronized!")
                 .hasMessageContaining("Error! Wrong ship location! Try again:");
+    }
+
+    @ParameterizedTest
+    @MethodSource("correctCoordinatesProvider")
+    void shouldCalculateDistanceWhenCoordinatesAreCorrect(Coordinates coords1, Coordinates coords2, int expectedDistance) {
+        // When
+        int distance = coords1.calculateDistanceTo(coords2);
+        // Then
+        Assertions.assertThat(distance)
+                .as("Should calculate distance")
+                .isEqualTo(expectedDistance);
+    }
+
+    private static Stream<Arguments> correctCoordinatesProvider() {
+        return Stream.of(
+                Arguments.of(new Coordinates(Row.A, new Column(1)), new Coordinates(Row.A, new Column(5)), 4),
+                Arguments.of(new Coordinates(Row.A, new Column(1)), new Coordinates(Row.E, new Column(1)), 4)
+        );
     }
 }
